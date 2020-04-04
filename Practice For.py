5ef2cfc49f10d4b81b1e380e0b81d93eb19bd731
Quang Vo <63116709+gerjunior402@users.noreply.github.com>
2020-04-04 08:28:28 +0700

    Add files via upload

---
+++ Practice For.py
@@ -0,0 +1,7 @@
+while True:
+    n = int(input("Input n:\t"))
+    print(n, 'So chan' if n%2 == 0 else 'So le')
+    t = str(input("Co tiep tuc hay khong? c/k"))
+    if t == 'k':
+        break;
+print('Cam on dung Software')
